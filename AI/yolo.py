1a5937517032175e1167db7841dddb58e4473587
Filip Lobpreis <f.lobpreis@gmail.com>
2025-11-04 23:28:26 +0100

    yolo
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- AI/yolo.py
+++ AI/yolo.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python3
+
 from ultralytics import YOLO
 
 # Create a new YOLO11n-OBB model from scratch
 model = YOLO("yolo11n-obb.yaml")
 
 # Train the model on the DOTAv1 dataset
-results = model.train(data="./YOLO/dataset.yaml", epochs=50, imgsz=640, batch=16)
+# https://docs.ultralytics.com/modes/train/#train-settings
+results = model.train(
+    data="./YOLO/dataset.yaml",
+    epochs=1000,
+    imgsz=1024,
+    batch=8,
+    # single_cls = True,
+    # classes = [ "potato" ],
+    # plots = True,
+    # close_mosaic = 0, # Disable mosaic augmentation in the last N epochs. 0 means disabled
+)
