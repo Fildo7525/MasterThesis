5456796bf9eb91cd2dcb8c7952963f388d5e966f
Filip Lobpreis <f.lobpreis@gmail.com>
2026-02-10 20:08:44 +0100

    AI: do not use the polygons that are not fully in the image
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- AI/yolo_qgis_converter.py
+++ AI/yolo_qgis_converter.py
@@ -488,6 +488,12 @@ class YOLOShapefileConverter:
                 minx, miny, maxx, maxy = clipped.bounds
                 width = maxx - minx
                 height = maxy - miny
+
+                # Check if the clipped is fully inside the original_polygon boundries
+                if not geom.within(original_polygon):
+                    print(f"Warning: Skipping annotation that is not fully within original tile bounds: {clipped.bounds}")
+                    continue
+
                 if clipped.is_empty or clipped.area == 0 or width < min_width or height < min_height:
                     print(f"Warning: Skipping {output_yolo_label} annotation with invalid geometry or area: {clipped.geom_type}")
                     continue
