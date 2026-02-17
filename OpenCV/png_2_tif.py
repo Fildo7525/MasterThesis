b9d5106daaec910ba76f2b9c477d50ae3221c051
Filip Lobpreis <f.lobpreis@gmail.com>
2026-02-17 22:35:54 +0100

    OpenCV: convert tif to png
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- OpenCV/png_2_tif.py
+++ OpenCV/png_2_tif.py
@@ -8,10 +8,7 @@ from pathlib import Path
 from typing import List
 
 
-
-
-def main(input_path: Path, reference_tif: Path, output_path: Path):
-
+def png2tif(input_path: Path, reference_tif: Path, output_path: Path):
     img = cv2.imread(str(input_path))
     if img is None:
         print(f"[ERROR]: Could not read image from path: {input_path}")
@@ -31,9 +28,42 @@ def main(input_path: Path, reference_tif: Path, output_path: Path):
             dst.write(bands[i], i + 1)
 
 
-if __name__ == "__main__":
-    input_path = Path("./tile_2_5_NRN_annotated.png")
-    reference_tif = Path("./tile_2_5_NRN.tif")
-    output_path = Path("./tile_2_5_NRN_annotated.tif")
+def tif2png(input_path: Path, output_path: Path):
+    with rasterio.open(input_path, "r") as src:
+        all_bands = src.read()
+        out_mat = []
 
-    main(input_path, reference_tif, output_path)
+        for i in range(all_bands.shape[0]):
+            band = all_bands[i, :, :].astype(np.float32)
+
+            # Min–max normalization
+            min_v = band.min()
+            max_v = band.max()
+
+            if max_v > min_v:
+                band = (band - min_v) / (max_v - min_v)
+            else:
+                band = np.zeros_like(band)
+
+            band = (band * 255).astype(np.uint8)
+            out_mat.append(band)
+
+        out_mat.reverse()
+        img = np.stack(out_mat, axis=0)
+        img = img.transpose(1, 2, 0)
+
+        print(f"Read TIF with shape: {img.shape}, dtype: {img.dtype}")
+        cv2.imwrite(str(output_path), img)
+
+
+
+if __name__ == "__main__":
+    # input_path = Path("./tile_2_5_NRN_annotated.png")
+    # reference_tif = Path("./tile_2_5_NRN.tif")
+    # output_path = Path("./tile_2_5_NRN_annotated.tif")
+
+    # png2tif(input_path, reference_tif, output_path)
+
+    input_path = Path("/home/fildo/SDU/MasterThesis/OpenCV/annotated_pngs/mid/tile_15_9.tif")
+    output_path = Path("/home/fildo/SDU/MasterThesis/OpenCV/annotated_pngs/mid/tile_15_9.png")
+    tif2png(input_path, output_path)
